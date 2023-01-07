94e35fc070a861b5ccf680800c5c3dd49e19e5ec
Francisco Possetto <franciscopossetto@hotmail.com>
2023-01-07 13:31:21 -0500

    replaced old api key and stored it in a env file

--- src/main/java/com/mapToFiction/mapToFiction/service/impl/GoogleMapsServiceImpl.java
+++ src/main/java/com/mapToFiction/mapToFiction/service/impl/GoogleMapsServiceImpl.java
@@ -4,16 +4,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mapToFiction.mapToFiction.model.dto.gmaps.ResultsDTO;
 import com.mapToFiction.mapToFiction.service.GoogleMapsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GoogleMapsServiceImpl implements GoogleMapsService {
-    private static final String API_KEY = "AIzaSyDovoelmYKYpUhNuP_tX8iwxuaXeA_NRXc";
     private static final String GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json?address={address}&types=locality&key={key}";
-
     private final RestTemplate restTemplate;
+    @Value("${GOOGLE_MAPS_API_KEY}")
+    private String API_KEY ;
 
     public GoogleMapsServiceImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
